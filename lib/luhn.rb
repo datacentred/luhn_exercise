befafceae5738a7377049d3c6840ce8618a08649
Sean Handley <sean.handley@gmail.com>
2016-05-17 16:28:24 +0100

    Partially completed in ~40 minutes.

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,5 +1,21 @@
 module LuhnCheck
-  def self.valid?(number)
-    
+  def self.doubled_up_array(number)
+    luhnArray = []
+    if odd? number 
+      number.to_s.chars.each_with_index do |oddChar, i|
+        if i.odd?
+          charTimesTwo = oddChar.to_i * 2
+          luhnArray << charTimesTwo
+        else
+          luhnArray << oddChar.to_i
+        end
+      end
+    end
+    luhnArray
   end
+
+  def self.odd?(number)
+    number.to_s.length % 2 != 0
+  end
+
 end
--- test/lib/luhn_test.rb
+++ test/lib/luhn_test.rb
@@ -2,11 +2,20 @@ require 'test_helper'
 
 class TestLuhnCheck < Minitest::Test
   def setup
-    @valid   = [2121, 1230, 4111111111111111, 26, 2626262626262626, 91, 912030]
-    @invalid = [123, 1, 8675309, 92, 922030, 111111111111111111111111]
+    @valid   = [2121, 1230, 4111111111111111, 26,
+                2626262626262626, 91, 912030]
+
+    @invalid = [123, 1, 8675309, 92, 922030,
+                111111111111111111111111]
   end
 
-  def test_something
-    flunk
+  def test_whether_odd_or_even
+    assert LuhnCheck.odd?(233)
+    refute LuhnCheck.odd?(23)
+  end
+
+  def test_whether_odd_number_times_by_two
+    assert_equal [1, 4, 3], LuhnCheck.doubled_up_array(123)
+    assert_equal [2, 8, 8], LuhnCheck.doubled_up_array(248)
   end
 end
